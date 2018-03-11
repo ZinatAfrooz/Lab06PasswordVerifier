8f1758d80c13f592282d191dd16e2048c232c63f
ZinatAfrooz <S01012776@acad.tri-c.edu>
2018-03-10 22:53:03 -0500

    Password Verifier

---
+++ Lab06PasswordVerifier/Lab06PasswordVerifier/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPassword = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblPassword
+        '
+        Me.lblPassword.AutoSize = True
+        Me.lblPassword.Location = New System.Drawing.Point(41, 95)
+        Me.lblPassword.Name = "lblPassword"
+        Me.lblPassword.Size = New System.Drawing.Size(81, 13)
+        Me.lblPassword.TabIndex = 0
+        Me.lblPassword.Text = "Enter Password"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(150, 95)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(100, 20)
+        Me.txtPassword.TabIndex = 1
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(110, 171)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 2
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtPassword)
+        Me.Controls.Add(Me.lblPassword)
+        Me.Name = "Form1"
+        Me.Text = "Password Verifier"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnSubmit As Button
+End Class
---
+++ Lab06PasswordVerifier/Lab06PasswordVerifier/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        Dim strPassword As String
+        Dim isValidPassword As Boolean
+
+        strPassword = txtPassword.Text
+
+        'verif if it is less than six charactres
+        If strPassword.Length < 6 Then
+            MessageBox.Show("The password should be at least six characters long")
+        Else
+            'calling the IsValid fucntion to veirfy the at least one numeric  and at least one alpha character
+            isValidPassword = IsValid(strPassword)
+            If isValidPassword = True Then
+                MessageBox.Show("The password is valid")
+            Else
+                MessageBox.Show("The password should contain at least one numeric digit and at least one alphabetic character")
+            End If
+        End If
+
+    End Sub
+
+    Function IsValid(ByVal strPassword As String) As Boolean
+        Dim boolNumric As Boolean
+        Dim boolAlphabetic As Boolean
+
+        For Each c As Char In strPassword
+            If Char.IsLetter(c) Then boolAlphabetic = True
+            If Char.IsNumber(c) Then boolNumric = True
+        Next
+        If boolAlphabetic And boolNumric Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Class
